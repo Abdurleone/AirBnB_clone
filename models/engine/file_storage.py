5a1e03fc5727963e77d7b11939676634b372ac88
Abdurleone <abyyyusuf@gmail.com>
2023-10-11 12:21:31 +0300

    bnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,7 @@ from models.user import User
 from models.state import State
 from models.place import Place
 from models.amenity import Amenity
-from  models.review import Review
+from models.review import Review
 
 
 class FileStorage:
